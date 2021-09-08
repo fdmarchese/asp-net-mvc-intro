22a2690f81237623dc8dd87dffd53066bc774a80
Federico Marchese <federico.d.marchese@gmail.com>
2021-09-08 12:40:45 -0300

    ajustes mínimos en clase

--- asp-net-mvc-intro/Startup.cs
+++ asp-net-mvc-intro/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -42,6 +38,7 @@ namespace asp_net_mvc_intro
 
             app.UseAuthorization();
 
+            // SISTEMA DE RUTAS
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
