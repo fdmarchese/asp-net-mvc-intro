98f114ae4efb5b8cc0647fc3a456404386e2c296
Federico Marchese <federico.d.marchese@gmail.com>
2021-09-22 15:31:18 -0300

    se agregan algunos comentarios en el código y ajustes menores

--- usando-entity-framework/Models/Alumno.cs
+++ usando-entity-framework/Models/Alumno.cs
@@ -4,8 +4,13 @@ namespace usando_entity_framework.Models
 {
     public class Alumno : Usuario
     {
+        // relación 1:1, o sea, un contacto se relaciona con un alumno y un alumno se relaciona con un contacto
         public Contacto Contacto { get; set; }
+
+        // propiedad de navegación para obtener todos los teléfonos relacionados al alumno
         public List<Telefono> Telefonos { get; set; }
+
+        // cardinalidad n:n, por eso se usa una entidad relacional intermedia llamada MateriaAlumno
         public List<MateriaAlumno> Materias { get; set; }
     }
 }
--- usando-entity-framework/Models/Usuario.cs
+++ usando-entity-framework/Models/Usuario.cs
@@ -5,12 +5,14 @@ namespace usando_entity_framework.Models
 {
     public abstract class Usuario
     {
+        // Esta forma es la correcta de especificar que Id va a ser la PK
         [Key]
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [MaxLength(100, ErrorMessage = "{0} admite un máximo de {1} caracteres")]
         [MinLength(2, ErrorMessage = "{0} debe tener un mínimo de {1} caracteres")]
+        // las RegularExpression solamente se aplican sobre properties de tipo string!!!
         [RegularExpression(@"[a-zA-Z áéíóú]*", ErrorMessage = "El campo {0} sólo admite caracteres alfabéticos")]
         public string Nombre { get; set; }
 
